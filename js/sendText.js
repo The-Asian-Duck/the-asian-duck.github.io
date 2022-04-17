396b4d8d9545c5bbee9f1b2d7995e68850f023e6
TheAsianDuck <yug201703@gmail.com>
2022-04-17 14:48:49 -0700

    added js functions.
    js functions:
     - normal sending message functionality
     - press enter to send, press send button to send
     - when message sent, it will be put into message box
    
     - added username box to show your user/the user to chat as id
    
    other changes:
     - small css changes and fixes

---
+++ js/sendText.js
@@ -0,0 +1,45 @@
+
+function checkMessageAmount() {
+    if (document.getElementsByClassName("msg").length !== 0){
+        if(document.getElementById("no-messages")){
+            const noMessageContainer = document.getElementById("no-messages").parentNode;
+            noMessageContainer.remove()
+        }
+    }
+}
+
+checkMessageAmount()
+
+function sendText(messageString){
+    if(messageString !== '' && messageString !== undefined){
+        const msgContainer = document.getElementById('messages')
+        const username = document.getElementById('username-input');
+        const messageElement = `
+        <div class="msg">
+            <span class="msg-username">${username.value} : </span>
+            <span class="msg-text">${messageString}</span>
+        </div>
+        `
+        msgContainer.insertAdjacentHTML('beforeend', messageElement)
+    }
+    checkMessageAmount()
+}
+
+const textInputElement = document.getElementById('text-input')
+
+textInputElement.addEventListener('keyup', event => {
+    if (event.key === 'Enter') {
+        sendText(textInputElement.value)
+        textInputElement.value = ''
+    }
+})
+
+const sendButtonElement = document.getElementById('send')
+
+sendButtonElement.addEventListener('click', event => {
+    sendText(textInputElement.value)
+    textInputElement.value = ''
+    textInputElement.focus()
+})
+
+console.log('js loaded')
