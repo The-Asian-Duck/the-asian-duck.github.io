f104139be162273665b42867a342b865e657dde6
TheAsianDuck <yug201703@gmail.com>
2022-04-23 11:51:35 -0700

    using fetch js api now + json data instead of query

--- js/sendText.js
+++ js/sendText.js
@@ -12,28 +12,37 @@ checkMessageAmount()
 
 function sendText(messageString){
 
+    const username = document.getElementById('username-input').value;
+
     const path = `73.220.38.85:3000/send?message=${messageString}`
 
+    const msgData = {
+        username: username,
+        msgString: messageString,
+    }
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(msgData),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    }
+
     if(messageString !== '' && messageString !== undefined){
-        axios.post(path).then(
-            (response) => {
-                var result = response.data;
-                console.log(result);
-            },
-            (error) => {
-                console.log(error);
-            }
-        );
+        fetch(path, options)
+            .then(res => res.json())
+            .then(res => console.log(res));
     }
 }
 
 function writeText(messageString){
     if(messageString !== '' && messageString !== undefined){
         const msgContainer = document.getElementById('messages')
-        const username = document.getElementById('username-input');
+        const username = document.getElementById('username-input').value;
         const messageElement = `
         <div class="msg">
-            <span class="msg-username">${username.value} : </span>
+            <span class="msg-username">${username} : </span>
             <span class="msg-text">${messageString}</span>
         </div>
         `
